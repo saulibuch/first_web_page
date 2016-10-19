0bd8872c18ebf845d67b0b6ff08a45b1833d587c
saulibuch <saulibuch@gmail.com>
2016-10-19 22:23:24 +0200

    next excersize

---
+++ Zed/ex7.py
@@ -0,0 +1,26 @@
+print ("Mary had a little lamb")
+# using formatter to put in some text
+print ("Its fleece was white as %s." %'snow')
+print ("And everywhere that mary went.")
+
+# repeating same string several times before printing
+print ("." * 10 ) #what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#whatch that comma at the end. try removing it to see what happens
+# that comma would work in Python 2
+# printing the variables 'end*'; as I need to stay on same line, end of string for print function os changed to emprty string. also space needs to be added before. 
+print ( end1 + end2 + end3 + end4 + end5 + end6 + ' ', end = '' )
+print ( end7 + end8 + end9 + end10 + end11 + end12 )
