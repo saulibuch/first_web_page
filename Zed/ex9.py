1cada54cffec8f5573268acc1668603e86ba4951
saulibuch <saulibuch@gmail.com>
2016-10-21 00:02:45 +0200

    new excercise and mistamex file in .txt

---
+++ Zed/ex9.py
@@ -0,0 +1,20 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# preapre string with days
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# prepare string with monts, use newline symbol as separator
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# print the days and motns
+print ( "Here are the days: ", days )
+print ("Here are the months: ", months )
+
+# print whole pagargraph
+print ("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+)
+
