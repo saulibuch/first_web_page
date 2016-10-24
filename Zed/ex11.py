97794f806b76dcf8f9a145f582e761dafc203e2e
saulibuch <saulibuch@gmail.com>
2016-10-24 21:19:40 +0200

    next ex

--- Zed/ex11.py
+++ Zed/ex11.py
@@ -1,1 +1,8 @@
-print ("How old are you?")
+print ("How old are you?", end = ' ')
+age = input()
+print ("Haw tall are you?", end = ' '),
+height = input()
+print ("How much do you weight?", end = ' '),
+weight = input()
+
+print ("So, you are %r old, %r tall and %r heavy." % (age, height, weight) )
