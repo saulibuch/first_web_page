a78cab120beb8d392211e320d535338e7851bcc5
saulibuch <saulibuch@gmail.com>
2016-11-13 22:48:26 +0100

    New wave

---
+++ Zed/ex19_2.py
@@ -0,0 +1,9 @@
+# -*- encoding: UTF-8 -*-
+
+def print_a_number(my_number):
+	print ("Well, you chose: %d for your number.\n" % my_number )
+#
+
+print_a_number (5)
+
+print_a_number (int(input("Insert new number:  ---> ")))
